46505648cbb4f80697f0ef6c8a6353708361a377
Thiago Monteiro <tsmonteiro@gmail.com>
2022-11-28 17:20:43 -0300

    init commit

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,10 @@
-FROM tercen/runtime-r40:4.0.4-4
+FROM tercen/runtime-python39:0.0.1
 #FROM tercen/runtime-r40-slim:4.0.4-0
-
-ENV RENV_VERSION 0.13.0
-RUN R -e "install.packages('remotes', repos = c(CRAN = 'https://cran.r-project.org'))"
-RUN R -e "remotes::install_github('rstudio/renv@${RENV_VERSION}')"
 
 COPY . /operator
 WORKDIR /operator
 
-RUN R -e "renv::consent(provided=TRUE);renv::restore(confirm=FALSE)"
-
 ENV TERCEN_SERVICE_URI https://tercen.com
 
-ENTRYPOINT [ "R","--no-save","--no-restore","--no-environ","--slave","-f","main.R", "--args"]
+ENTRYPOINT [ "python3","main.py"]
 CMD [ "--taskId", "someid", "--serviceUri", "https://tercen.com", "--token", "sometoken"]
---
+++ main.py
@@ -0,0 +1,22 @@
+# python3 -m venv -p /home/thiago/micromamba/bin/python3 venv
+# virtualenv venv -p /usr/bin/python3.9
+# python3 -m pip install wheel
+# python3 -m pip install git+https://github.com/tercen/tercen_python_client
+from tercen.client import context as ctx
+import numpy as np
+
+# http://127.0.0.1:5402/test/w/9b611b90f412969d6f617f559f005bc6/ds/2ca54ff5-5b7f-44e9-870b-48facabc41ae
+# tercenCtx = ctx.TercenContext(workflowId = '9b611b90f412969d6f617f559f005bc6', 
+#                         stepId = '2ca54ff5-5b7f-44e9-870b-48facabc41ae')
+
+tercenCtx = ctx.TercenContext()
+
+df = tercenCtx.select(['.y', '.ci', '.ri'])
+
+dfGroup = df.groupby(['.ci','.ri'], as_index=False)
+df['mean'] = dfGroup.agg({'.y':'mean'}).rename(columns={'.y':'mean'})['mean']
+df = df.drop('.y', axis=1)
+
+df = tercenCtx.add_namespace(df) 
+tercenCtx.save(df)
+
