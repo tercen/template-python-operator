a4ffb4189be2c81506640a4f3efe629c45ba7fe0
Thiago Monteiro <tsmonteiro@gmail.com>
2022-12-05 09:19:46 -0300

    fix for ci, ri grouping

--- main.py
+++ main.py
@@ -8,15 +8,14 @@ import numpy as np
 
 # http://127.0.0.1:5402/test/w/9b611b90f412969d6f617f559f005bc6/ds/2ca54ff5-5b7f-44e9-870b-48facabc41ae
 # tercenCtx = ctx.TercenContext(workflowId = '9b611b90f412969d6f617f559f005bc6', 
-#                          stepId = '2ca54ff5-5b7f-44e9-870b-48facabc41ae')
+#                           stepId = '2ca54ff5-5b7f-44e9-870b-48facabc41ae')
 
 tercenCtx = ctx.TercenContext()
 
 df = tercenCtx.select(['.y', '.ci', '.ri'])
 
-dfGroup = df.groupby(['.ci','.ri'], as_index=False)
-df['mean'] = dfGroup.agg({'.y':'mean'}).rename(columns={'.y':'mean'})['mean']
-df = df.drop('.y', axis=1)
+df = df.groupby(['.ci','.ri'], as_index=False).mean()
+df = df.rename(columns={".y":"mean"})
 
 df = tercenCtx.add_namespace(df) 
 tercenCtx.save(df)
